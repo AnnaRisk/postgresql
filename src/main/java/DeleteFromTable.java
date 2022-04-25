26c17d40f1827c1211fc5298a44d49f6025e05e0
24-002580\user <annarainchik@gmail.com>
2022-04-25 21:59:21 +0300

    test

---
+++ src/main/java/DeleteFromTable.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteFromTable {
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(Config.DB_URL, Config.USER, Config.PASSWORD);
+             Statement statement = connection.createStatement()) {
+            String sql = "delete from Students where id = 2;";
+            statement.executeUpdate(sql);
+            System.out.println("Record was deleted");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
